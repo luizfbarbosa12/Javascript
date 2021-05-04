80f6cdbc927b5d0f7a88ae27b3b49bb7436f19d9
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2021-05-04 17:09:47 -0300

    finished

--- timer/app.js
+++ timer/app.js
@@ -1,14 +1,42 @@
-const timer = document.querySelector('#timer')
-const start = document.querySelector('#start')
-const pause = document.querySelector('#pause')
-const zero = document.querySelector('#zero')
+//new Date(0); = data inicial/timestamp zero.
 
-start.addEventListener('click', (event) => {
-    console.log('clicou no iniciar')
-})
-pause.addEventListener('click', (event) => {
-    console.log('clicou no pausar')
-})
-zero.addEventListener('click', (event) => {
-    console.log('clicou no zerar')
-})
+const getTimeSeconds = (seconds) => {
+  const data = new Date(seconds * 1000);
+  return data.toLocaleTimeString("pt-BT", { hour12: false, timeZone: "GMT" });
+};
+
+const timer = document.querySelector("#timer");
+const start = document.querySelector(".start");
+const pause = document.querySelector(".pause");
+const zero = document.querySelector(".zero");
+let seconds = 0;
+let time;
+
+const startClock = () => {
+  time = setInterval(function () {
+    seconds++;
+    timer.innerHTML = getTimeSeconds(seconds);
+  }, 1000);
+};
+
+document.addEventListener("click", function (event) {
+  const element = event.target;
+  if (element.classList.contains("zero")) {
+    clearInterval(time);
+    timer.innerHTML = "00:00:00";
+    seconds = 0;
+    timer.classList.remove("paused");
+  }
+
+  if (element.classList.contains("start")) {
+    timer.classList.remove("paused");
+    clearInterval(time);
+    startClock();
+  }
+
+  if (element.classList.contains("pause")) {
+    clearInterval(time);
+    timer.classList.add("paused");
+  }
+});
+
